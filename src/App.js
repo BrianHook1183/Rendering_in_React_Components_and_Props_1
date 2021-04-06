10e1c2970d85971f100413d7a085bf45ce02453e
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-04-06 03:30:40 -0500

    solution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Holiday from "./Holiday";
 
-function App() {}
+function App() {
+  return (
+    <div>
+      <Holiday name="Valentine's Day" day="14" month="February" />
+      <Holiday name="Christmas" day="25" month="December" />
+    </div>
+  );
+}
 
 export default App;
